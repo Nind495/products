37f85db77eb9f2b57a08c329f40b8910e1f3e2e5
Nind495 <31470148+Nind495@users.noreply.github.com>
2017-09-13 18:16:28 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+select * from product
+update product set Nnomer=123, name = 'asdfads', prise = 34 where idproduct = 59
+update product set Nnomer =:, name =:name , prise =:prise
+delete from product
